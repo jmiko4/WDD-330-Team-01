8ba4c124b80b7519a70c2c2d66313a9cc8442d4e
jmiko4 <mik19003@byui.edu>
2024-05-06 15:42:42 -0600

    buiilt and linted aAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,8 @@
-import {getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 import productDetails from "./productDetails.mjs";
 import { addProductToCart } from "./productDetails.mjs";
-const productId = getParam('product');
+const productId = getParam("product");
 productDetails(productId);
 
 // add to cart button event handler
