656e9daee119cc9669f0e737af0b2c2568a8cf09
jmiko4 <mik19003@byui.edu>
2024-05-29 16:05:07 -0600

    finished team5

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,35 +1,26 @@
-// productList.mjs
+import { getData } from "./productData.mjs";
+import { renderListWithTemplate } from "./utils.mjs";
 
-import { getData } from './productData.mjs';
-import { renderListWithTemplate } from './utils.mjs';
-
-// Function to generate the HTML template for a product card
 function productCardTemplate(product) {
-    return `<li class="product-card">
-    <a href="product_pages/index.html?product=${product.Id}">
+  return `<li class="product-card">
+    <a href="/product_pages/index.html?product=${product.Id}">
     <img
-      src="${product.Image}"
+      src="${product.Images.PrimaryMedium}"
       alt="Image of ${product.Name}"
     />
     <h3 class="card__brand">${product.Brand.Name}</h3>
-    <h2 class="card__name">${product.Name}</h2>
+    <h2 class="card__name">${product.NameWithoutBrand}</h2>
     <p class="product-card__price">$${product.FinalPrice}</p></a>
   </li>`;
 }
 
-// Function to filter the list of products to include only the required tents
-function filterTents(products) {
-    const tentIds = ['880RR', '985RF', '344YJ', '985PR'];
-    return products.filter(product => tentIds.includes(product.Id));
-}
-
-// Function to generate the list of products based on category
-export default function productList(selector, category) {
-    getData(category)
-        .then(products => {
-            const filteredProducts = filterTents(products);
-            const productListElement = document.querySelector(selector);
-            renderListWithTemplate(productCardTemplate, productListElement, filteredProducts);
-        })
-        .catch(error => console.error('Error fetching product data:', error));
+export default async function productList(selector, category) {
+  // get the element we will insert the list into from the selector
+  const el = document.querySelector(selector);
+  // get the list of products
+  const products = await getData(category);
+  console.log(products);
+  // render out the product list to the element
+  renderListWithTemplate(productCardTemplate, el, products);
+  document.querySelector(".title").innerHTML = category.charAt(0).toUpperCase() + category.slice(1);
 }
